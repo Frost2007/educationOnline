c4aab850ff0866fa8b7d7f9893b7b72704ac5da5
Frost2007 <vip.psn2007@mail.ru>
2022-06-21 20:50:46 +0400

    Creating new project

---
+++ src/test/java/com/example/musicschool/MusicSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musicschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
