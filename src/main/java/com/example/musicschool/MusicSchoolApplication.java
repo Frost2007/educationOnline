d46e5e0eabc46e787f3ca371371d700ec5798679
Hayk <haykavetisyan882@gmail.com>
2022-06-22 23:46:13 +0400

    First Commit of Hayk Avetisyan

--- src/main/java/com/example/musicschool/MusicSchoolApplication.java
+++ src/main/java/com/example/musicschool/MusicSchoolApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MusicSchoolApplication {
-    // TODO
+    // TODO dhjgh
 
     public static void main(String[] args) {
 
